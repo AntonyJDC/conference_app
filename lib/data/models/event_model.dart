d76db666224e5687f7450e720d83f82e2817b163
Antony Dominguez <jaconsuegra@uninorte.edu.co>
2025-03-23 00:11:53 -0500

    Enhance EventModel with additional fields for start time, end time, capacity, spots left, and categories; update dummy events data accordingly

--- lib/data/models/event_model.dart
+++ lib/data/models/event_model.dart
@@ -1,10 +1,15 @@
 class EventModel {
   final String id;
-  final String title;
-  final String description;
-  final String imageUrl;
-  final String date; // Formato "YYYY-MM-DD"
-  final String location;
+  final String title; // Nombre del evento
+  final String description; // Descripción
+  final String imageUrl; // Imagen
+  final String date; // Fecha del evento "YYYY-MM-DD"
+  final String startTime; // Hora de inicio "HH:MM"
+  final String endTime; // Hora de fin "HH:MM"
+  final String location; // Ubicación (dirección)
+  final int capacity; // Capacidad máxima de asistentes
+  int spotsLeft; // Cupos disponibles
+  final List<String> categories; // Categorías (Ej: Música, Tecnología)
 
   EventModel({
     required this.id,
@@ -12,7 +17,12 @@ class EventModel {
     required this.description,
     required this.imageUrl,
     required this.date,
+    required this.startTime,
+    required this.endTime,
     required this.location,
+    required this.capacity,
+    required this.spotsLeft,
+    required this.categories,
   });
 
   factory EventModel.fromJson(Map<String, dynamic> json) => EventModel(
@@ -21,6 +31,11 @@ class EventModel {
         description: json['description'],
         imageUrl: json['image_url'],
         date: json['date'],
+        startTime: json['start_time'],
+        endTime: json['end_time'],
         location: json['location']['address'],
+        capacity: json['capacity'],
+        spotsLeft: json['spots_left'],
+        categories: List<String>.from(json['categories']),
       );
 }
