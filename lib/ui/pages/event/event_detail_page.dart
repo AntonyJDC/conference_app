917e79df421f70e36d3079e73830715fb0265e00
Antony Dominguez <jaconsuegra@uninorte.edu.co>
2025-03-27 22:25:47 -0500

    Refactor EventDetailPage and SubscribeButton to improve event loading and subscription logic

--- lib/ui/pages/event/event_detail_page.dart
+++ lib/ui/pages/event/event_detail_page.dart
@@ -1,16 +1,17 @@
 import 'package:conference_app/data/models/event_model.dart';
+import 'package:conference_app/domain/use_case/get_event_by_id_use_case.dart';
+import 'package:conference_app/ui/pages/event/widgets/event_image.dart';
+import 'package:conference_app/ui/pages/event/widgets/event_info.dart';
+import 'package:conference_app/ui/pages/event/widgets/subscribe_button.dart';
+import 'package:conference_app/controllers/favorite_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'widgets/event_image.dart';
-import 'widgets/event_info.dart';
-import 'widgets/subscribe_button.dart';
-import 'package:conference_app/controllers/favorite_controller.dart';
 
 class EventDetailPage extends StatefulWidget {
   const EventDetailPage({super.key});
 
   @override
-  EventDetailPageState createState() => EventDetailPageState();
+  State<EventDetailPage> createState() => EventDetailPageState();
 }
 
 class EventDetailPageState extends State<EventDetailPage> {
@@ -22,44 +23,57 @@ class EventDetailPageState extends State<EventDetailPage> {
     super.initState();
     favoriteController = Get.find<FavoriteController>();
 
-    // Convertir el evento en un observable
-    event = (Get.arguments as EventModel).obs;
+    final initialEvent = Get.arguments as EventModel;
+    event = initialEvent.obs;
+
+    _loadEventFromDb(initialEvent.id);
+  }
+
+  Future<void> _loadEventFromDb(String id) async {
+    final eventFromDb = await GetEventByIdUseCase().execute(id);
+    if (eventFromDb != null) {
+      event.value = eventFromDb;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.surface,
-      body: Stack(
-        children: [
-          ListView(
-            padding: const EdgeInsets.only(bottom: 20),
-            children: [
-              EventImage(event: event()),
-              EventInfo(event: event()),
-            ],
-          ),
-          Positioned(
-            top: 40,
-            left: 16,
-            child: _circleButton(() => Get.back(), Icons.arrow_back),
-          ),
-          Positioned(
-            top: 40,
-            right: 16,
-            child: Obx(() {
-              bool isFav = favoriteController.isFavorite(event.value);
-              return _circleButton(
-                () => favoriteController.toggleFavorite(event.value),
-                isFav ? Icons.favorite : Icons.favorite_border,
-                color: isFav ? Colors.red : Colors.white,
-              );
-            }),
-          ),
-          SubscribeButton(event: event),
-        ],
-      ),
-    );
+    return Obx(() {
+      final eventData = event.value;
+
+      return Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        body: Stack(
+          children: [
+            ListView(
+              padding: const EdgeInsets.only(bottom: 20),
+              children: [
+                EventImage(event: eventData),
+                EventInfo(event: event),
+              ],
+            ),
+            Positioned(
+              top: 40,
+              left: 16,
+              child: _circleButton(() => Get.back(), Icons.arrow_back),
+            ),
+            Positioned(
+              top: 40,
+              right: 16,
+              child: Obx(() {
+                bool isFav = favoriteController.isFavorite(event.value);
+                return _circleButton(
+                  () => favoriteController.toggleFavorite(event.value),
+                  isFav ? Icons.favorite : Icons.favorite_border,
+                  color: isFav ? Colors.red : Colors.white,
+                );
+              }),
+            ),
+            SubscribeButton(event: event),
+          ],
+        ),
+      );
+    });
   }
 
   Widget _circleButton(VoidCallback onTap, IconData icon,
@@ -69,7 +83,7 @@ class EventDetailPageState extends State<EventDetailPage> {
       child: Container(
         padding: const EdgeInsets.all(8),
         decoration: BoxDecoration(
-          color: Colors.black.withValues(alpha: 0.5),
+          color: Colors.black.withAlpha(100),
           shape: BoxShape.circle,
         ),
         child: Icon(icon, color: color),
--- lib/ui/pages/event/widgets/event_info.dart
+++ lib/ui/pages/event/widgets/event_info.dart
@@ -1,159 +1,108 @@
-import 'dart:async';
 import 'package:conference_app/data/models/event_model.dart';
+import 'package:conference_app/domain/use_case/check_event_status_use_case.dart';
 import 'package:conference_app/ui/pages/event/widgets/detail_icon.dart';
 import 'package:conference_app/ui/pages/event/widgets/event_category_tags.dart';
 import 'package:conference_app/ui/pages/reviews/widgets/reviews_carousel.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:intl/intl.dart';
-import 'package:timezone/timezone.dart' as tz;
-import 'package:timezone/data/latest.dart' as tz;
 
-class EventInfo extends StatefulWidget {
-  final EventModel event;
+class EventInfo extends StatelessWidget {
+  final Rx<EventModel> event;
   final VoidCallback? onTap;
 
   const EventInfo({super.key, required this.event, this.onTap});
 
   @override
-  State<EventInfo> createState() => _EventInfoState();
-}
-
-class _EventInfoState extends State<EventInfo> {
-  late Timer _timer;
-  late tz.Location _colombiaTZ;
-  bool isPastEvent = false;
-
-  @override
-  void initState() {
-    super.initState();
-    tz.initializeTimeZones();
-    _colombiaTZ = tz.getLocation('America/Bogota');
-    _checkEventStatus();
-
-    // 🔄 Actualiza cada minuto por si el evento finaliza
-    _timer = Timer.periodic(const Duration(seconds: 10), (_) {
-      if (mounted) _checkEventStatus();
-    });
-  }
-
-  void _checkEventStatus() {
-    final eventDate = DateTime.tryParse(widget.event.date);
-    if (eventDate != null) {
-      final endParts = widget.event.endTime.split(':');
-      final eventEnd = tz.TZDateTime(
-        _colombiaTZ,
-        eventDate.year,
-        eventDate.month,
-        eventDate.day,
-        int.parse(endParts[0]),
-        int.parse(endParts[1]),
-      );
-
-      setState(() {
-        isPastEvent = tz.TZDateTime.now(_colombiaTZ)
-            .isAfter(eventEnd.subtract(const Duration(seconds: 2)));
-      });
-    }
-  }
-
-  @override
-  void dispose() {
-    _timer.cancel();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
+    final theme = Theme.of(context).colorScheme;
     final size = MediaQuery.of(context).size;
     final overlap = size.height * 0.35 * 0.15;
 
-    final double percentage = widget.event.spotsLeft / widget.event.capacity;
-    Color spotColor = percentage < 0.25
-        ? Colors.red
-        : percentage < 0.5
-            ? Colors.amber
-            : Colors.green;
+    return Obx(() {
+      final e = event.value;
+      final percentage = e.spotsLeft / e.capacity;
 
-    final eventDate = DateTime.tryParse(widget.event.date);
+      final spotColor = percentage < 0.25
+          ? Colors.red
+          : percentage < 0.5
+              ? Colors.amber
+              : Colors.green;
 
-    return Transform.translate(
-      offset: Offset(0, -overlap),
-      child: Container(
-        padding: const EdgeInsets.all(18),
-        decoration: BoxDecoration(
-          color: theme.colorScheme.surface,
-          borderRadius: const BorderRadius.vertical(top: Radius.circular(24)),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(widget.event.title,
-                style: theme.textTheme.titleMedium
-                    ?.copyWith(fontWeight: FontWeight.bold)),
-            Text(widget.event.description,
-                style: TextStyle(color: Colors.grey.shade600, fontSize: 12)),
-            const SizedBox(height: 25),
+      final isPastEvent = CheckEventStatusUseCase().execute(e);
+      final eventDate = DateTime.tryParse(e.date);
 
-            // 📅 FECHA Y HORA o MENSAJE si finalizó
-            DetailIcon(
-              icon: Icons.calendar_today,
-              title: widget.event.date,
-              subtitle: isPastEvent
-                  ? 'Este evento ya finalizó'
-                  : '${DateFormat('EEEE', 'es_CO').format(eventDate!)}, de ${widget.event.startTime} a ${widget.event.endTime}',
-              subtitleStyle: TextStyle(
-                color: isPastEvent ? Colors.red : Colors.grey,
-                fontSize: 10,
-              ),
-            ),
-
-            const SizedBox(height: 16),
-            DetailIcon(
-              icon: Icons.location_on,
-              title: widget.event.location,
-              subtitle: "Ubicación",
-              subtitleStyle: null,
-            ),
-            const SizedBox(height: 16),
-            DetailIcon(
-              icon: Icons.people,
-              title: "${widget.event.capacity} personas",
-              subtitle: "Capacidad",
-              subtitleStyle: null,
-            ),
-
-            const SizedBox(height: 24),
-
-            // ✅ Mostrar Cupos disponibles solo si el evento no ha pasado
-            if (!isPastEvent)
-              _spotsAvailable(context, widget.event.spotsLeft, spotColor),
-
-            const SizedBox(height: 24),
-            EventCategoryTags(event: widget.event),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("Reseñas",
-                    style: const TextStyle(
-                        fontSize: 15, fontWeight: FontWeight.bold)),
-                GestureDetector(
-                  onTap: widget.onTap,
-                  child: Text("Ver todas",
-                      style: TextStyle(
-                          fontSize: 11,
-                          fontWeight: FontWeight.w600,
-                          color: Theme.of(context).colorScheme.primary)),
+      return Transform.translate(
+        offset: Offset(0, -overlap),
+        child: Container(
+          padding: const EdgeInsets.all(18),
+          decoration: BoxDecoration(
+            color: theme.surface,
+            borderRadius: const BorderRadius.vertical(top: Radius.circular(24)),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(e.title,
+                  style: const TextStyle(
+                      fontSize: 16, fontWeight: FontWeight.bold)),
+              Text(e.description,
+                  style: TextStyle(color: Colors.grey.shade600, fontSize: 12)),
+              const SizedBox(height: 25),
+              DetailIcon(
+                icon: Icons.calendar_today,
+                title: e.date,
+                subtitle: isPastEvent
+                    ? 'Este evento ya finalizó'
+                    : '${DateFormat('EEEE', 'es_CO').format(eventDate!)}, de ${e.startTime} a ${e.endTime}',
+                subtitleStyle: TextStyle(
+                  color: isPastEvent ? Colors.red : Colors.grey,
+                  fontSize: 10,
                 ),
-              ],
-            ),
-            const SizedBox(height: 16),
-            const ReviewsCarousel(),
-          ],
+              ),
+              const SizedBox(height: 16),
+              DetailIcon(
+                icon: Icons.location_on,
+                title: e.location,
+                subtitle: "Ubicación",
+                subtitleStyle: null,
+              ),
+              const SizedBox(height: 16),
+              DetailIcon(
+                icon: Icons.people,
+                title: "${e.capacity} personas",
+                subtitle: "Capacidad",
+                subtitleStyle: null,
+              ),
+              const SizedBox(height: 24),
+              if (!isPastEvent)
+                _spotsAvailable(context, e.spotsLeft, spotColor),
+              const SizedBox(height: 24),
+              EventCategoryTags(event: e),
+              const SizedBox(height: 24),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text("Reseñas",
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+                  GestureDetector(
+                    onTap: onTap,
+                    child: Text("Ver todas",
+                        style: TextStyle(
+                            fontSize: 11,
+                            fontWeight: FontWeight.w600,
+                            color: theme.primary)),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+              const ReviewsCarousel(),
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 
   Widget _spotsAvailable(BuildContext context, int spotsLeft, Color color) {
