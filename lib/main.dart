221b55379685a65a5b348e51419564a848dedd04
Brancys <brancysbuninorte.edu.co>
2025-03-28 17:04:09 -0500

    Set preferred device orientation to portrait mode in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:conference_app/controllers/booked_events_controller.dart';
 import 'package:conference_app/controllers/favorite_controller.dart';
 import 'package:conference_app/domain/use_case/initialize_events_use_case.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:conference_app/routes/routes.dart';
 import 'package:conference_app/ui/theme/app_colors.dart';
@@ -9,6 +10,9 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp, // Bloquea en retrato hacia arriba
+  ]);
   WidgetsFlutterBinding.ensureInitialized();
   final prefs = await SharedPreferences.getInstance();
   final onboardingSeen = prefs.getBool('onboarding_seen') ?? false;
