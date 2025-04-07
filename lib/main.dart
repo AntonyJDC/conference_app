ec054c317ebfa1240e095cc975c671746a22f425
Antony Dominguez <jaconsuegra@uninorte.edu.co>
2025-04-06 22:30:54 -0500

    Refactor main.dart to load saved theme mode and pass it to MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,14 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'package:conference_app/controllers/theme_controller.dart';
 
 void main() async {
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp, // Bloquea en retrato hacia arriba
-  ]);
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   WidgetsFlutterBinding.ensureInitialized();
+
   final prefs = await SharedPreferences.getInstance();
   final onboardingSeen = prefs.getBool('onboarding_seen') ?? false;
+
+  // Cargar el tema guardado de SharedPreferences
+  final savedThemeMode = prefs.getString('theme_mode');
   final themeController = Get.put(ThemeController());
 
   Get.put(BookedEventsController());
@@ -27,22 +29,40 @@ void main() async {
   await themeController.loadTheme();
   await initializeDateFormatting('es_CO', null);
   await InitializeEventsUseCase().execute();
-  runApp(MyApp(onboardingSeen: onboardingSeen));
+
+  runApp(MyApp(
+    onboardingSeen: onboardingSeen,
+    savedThemeMode: savedThemeMode,
+  ));
 }
 
 class MyApp extends StatelessWidget {
   final bool onboardingSeen;
-  const MyApp({super.key, required this.onboardingSeen});
+  final String? savedThemeMode;
+
+  const MyApp({super.key, required this.onboardingSeen, this.savedThemeMode});
 
   @override
   Widget build(BuildContext context) {
     final materialTheme = MaterialTheme(ThemeData().textTheme);
 
+    // Determinar el themeMode basado en el valor guardado
+    ThemeMode themeMode;
+    if (savedThemeMode == 'dark') {
+      themeMode = ThemeMode.dark;
+    } else if (savedThemeMode == 'light') {
+      themeMode = ThemeMode.light;
+    } else {
+      themeMode = ThemeMode
+          .system; // Si no hay tema guardado, usar el predeterminado del sistema
+    }
+
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       initialRoute: onboardingSeen ? AppRoutes.main : AppRoutes.onboarding,
       getPages: AppRoutes.routes,
-      themeMode: ThemeMode.system,
+      themeMode:
+          themeMode, // Usar el tema guardado o predeterminado del sistema
 
       // 🌞 Tema Claro
       theme: materialTheme.light().copyWith(
